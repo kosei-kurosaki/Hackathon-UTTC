c4d3a787b1e72eba5fb0bc07290f93f67ee3ce1d
kosei_kurosaki <kosei0275@gmail.com>
2023-06-09 16:17:27 +0900

    addgo17

--- Hackathon/Go/main.go
+++ Hackathon/Go/main.go
@@ -28,7 +28,7 @@ func init() {
 	mysqlDatabase := os.Getenv("MYSQL_DATABASE")
 	//1-2
 
-	_db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@%s/%s", mysqlUser, mysqlUserPwd, mysqlHost, mysqlDatabase))
+	_db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@%s/%s?parseTime=true", mysqlUser, mysqlUserPwd, mysqlHost, mysqlDatabase))
 	if err != nil {
 		log.Fatalf("fail: sql.Open, %v\n", err)
 	}
